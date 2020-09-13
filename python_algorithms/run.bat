54ead599a95cd6e77cc8be192a4e232b7ed4d9f5
Yasmin van Dijk <vandijk.yasmin@gmail.com>
2020-09-13 23:30:37 +0200

    added offline and online basic greedy algorithms with comments / documentation + batch file to run the code

---
+++ python_algorithms/run.bat
@@ -0,0 +1,7 @@
+REM REM offline basic greedy algorithm
+REM python offline_basic_greedy.py < ./../test_instances/offline/offline_001.txt
+REM pause
+
+REM online basic greedy algorithm
+python online_basic_greedy.py < ./../test_instances/online/online_001.txt
+pause
