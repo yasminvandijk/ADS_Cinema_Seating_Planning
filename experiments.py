74861f8eb4b9b6832f888752fc94d52ee5e86a82
geo-j <g.juglan@tutanota.com>
2020-10-07 11:40:26 +0200

    add experiments script

--- experiments.py
+++ experiments.py
@@ -2,45 +2,63 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 import numpy as np
+import subprocess
+import time
 
 TEST_DIR = 'TestCases/'
 ALGORITHM_DIR = 'python_algorithms/'
 
 if __name__ == '__main__':
-    dataframe = pd.DataFrame(columns = ['test_instance', 'nr_people_seated', 'nr_places_unavailable'])
 
     for algorithm in listdir(ALGORITHM_DIR):
-        if '.py' in algorithm:
+        if '.py' in algorithm and 'ilp' not in algorithm:
+            dataframe = pd.DataFrame(columns = ['test_instance', 'nr_people_seated', 'nr_unavailable_places', 'running_time'])
+            print(f'running {algorithm[:-3]}')
+
+            for f in listdir(TEST_DIR):
+                if isfile(join(TEST_DIR, f)):
+                    with open(join(TEST_DIR, f)) as file:
+                        if ('online' in algorithm and 'Online' in f) or ('offline' in algorithm and 'Exact' in f):
+                            try:
+                                print(f'\t with test case {f[:-4]}')
+                                start = time.time()
+                                output = subprocess.check_output(['python3', join(ALGORITHM_DIR, algorithm)], stdin=file)
+                                end = time.time()
+                                output = output.split()
             
-    for f in listdir(TEST_DIR):
-        if isfile(join(TEST_DIR, f)):
-            with open(join(TEST_DIR, f)) as file:
-                print(f)
-                nrRows = int(file.readline())
-                nrCols = int(file.readline())
-                print(nrRows, nrCols)
+                                dataframe = dataframe.append({'test_instance': f[:-4], 'nr_people_seated': output[0], 'nr_unavailable_places': output[1], 'running_time': end - start}, ignore_index = True)
 
-                # cinema layout
-                layout = np.empty((nrRows, nrCols), dtype = str)
-                for i in range(nrRows):
-                    layout[i] = [b for b in file.readline().strip()]
+                                print(dataframe)
+                            except KeyboardInterrupt:
+                                dataframe = dataframe.append({'test_instance': f[:-4], 'nr_people_seated': 'N\A', 'nr_unavailable_places': 'N\A', 'running_time': 'N\A'}, ignore_index = True)
+                                pass
 
-                if 'Exact' in f:
-                # number of groups for each group size
-                    nrGroupsTotal = np.array([int(nr) for nr in file.readline().split()])
+            dataframe.to_csv(algorithm[:-3] + '.csv')
+            
+    #             print(f)
+    #             nrRows = int(file.readline())
+    #             nrCols = int(file.readline())
+    #             print(nrRows, nrCols)
 
-                    nrTotalPeople = 0
-                    for i in range(len(nrGroupsTotal)):
-                        nrTotalPeople += (i + 1) * nrGroupsTotal[i]
+    #             # cinema layout
+    #             layout = np.empty((nrRows, nrCols), dtype = str)
+    #             for i in range(nrRows):
+    #                 layout[i] = [b for b in file.readline().strip()]
+
+    #             if 'Exact' in f:
+    #             # number of groups for each group size
+    #                 nrGroupsTotal = np.array([int(nr) for nr in file.readline().split()])
+
+    #                 nrTotalPeople = 0
+    #                 for i in range(len(nrGroupsTotal)):
+    #                     nrTotalPeople += (i + 1) * nrGroupsTotal[i]
                     
-                else:
-                    nrTotalPeople = 0
-                    nr = file.readline().strip()
-                    while nr != '0':
-                        print(nr)
-                        nrTotalPeople += int(nr)
-                        nr = file.readline().strip()
+    #             else:
+    #                 nrTotalPeople = 0
+    #                 nr = file.readline().strip()
+    #                 while nr != '0':
+    #                     print(nr)
+    #                     nrTotalPeople += int(nr)
+    #                     nr = file.readline().strip()
 
-                dataframe = dataframe.append({'test_instance': f[:-4], 'cinema_size': nrRows * nrCols, 'nr_seats': np.count_nonzero(layout == '1'), 'nr_coming_people': nrTotalPeople}, ignore_index = True)
     
-    dataframe.to_csv('test_instances_data.csv')
